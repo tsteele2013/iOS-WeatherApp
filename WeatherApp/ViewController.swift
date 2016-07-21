8c8e1b1b068bda7fa014e0d476cac5d894c8c284
Trevor Steele <aforcesteele@gmail.com>
2016-07-20 20:30:40 -0400

    test commit

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -7,19 +7,58 @@
 //
 
 import UIKit
+import MapKit
+import Alamofire
+import SwiftyJSON
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    var lon = CLLocationDegrees()
+    var lat = CLLocationDegrees()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        print(locationManager.location?.coordinate)
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func getMyLatAndLon(sender: AnyObject) {
+        print(locationManager.location?.coordinate)
+        lat = 39.680833
+        lon = -86.1446394
+        let key = "e492180da724ac1e28d7cd6846bb98c0"
+        let url = "https://api.forecast.io/forecast/\(key)/\(lat),\(lon)"
+        
+        Alamofire.request(.GET, url)
+            .responseJSON(completionHandler: { Response in
+                if let data = Response.data {
+                    do {
+                        let json = try NSJSONSerialization.JSONObjectWithData(data, options: .MutableContainers) as! [ String : AnyObject]
+                        print(json)
+                    } catch {
+                        print("Error bitch")
+                        print("Bsm")
+                    }
+                }
+            })
     }
-
-
+    
 }
 
+//MARK: CLLocationManagerDelegate
+extension ViewController {
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            print("Found user's location: \(location.coordinate)")
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print("Failed to find user's location: \(error.localizedDescription)")
+    }
+}
