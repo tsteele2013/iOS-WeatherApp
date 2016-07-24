dc97ae9754d9665122643ade5e7893d87e168fa1
tsteele2013 <tsteele@elevenfiftyconsulting.com>
2016-07-24 15:22:12 -0400

    Major refactoring. Added HourlyWeatherItem struct.

---
+++ WeatherApp/HourlyWeatherItem.swift
@@ -0,0 +1,31 @@
+//
+//  HourlyWeatherItem.swift
+//  WeatherApp
+//
+//  Created by Trevor Steele on 7/24/16.
+//  Copyright © 2016 Trevor Steele. All rights reserved.
+//
+
+import Foundation
+
+struct HourlyWeatherItem {
+    
+    var hourlyTime: String?
+    var temperature: Int
+    var humidity: Double
+    var precipProbability: Double
+    //    var icon: UIImage
+    var windSpeed: Double
+    
+    init (hourlyWeather: NSArray, index: Int) {
+        
+        temperature = hourlyWeather[index]["temperature"] as! Int
+        humidity = hourlyWeather[index]["humidity"] as! Double
+        precipProbability = hourlyWeather[index]["precipProbability"] as! Double
+        windSpeed = hourlyWeather[index]["windSpeed"] as! Double
+        let hourlyTimeIntValue = hourlyWeather[index]["time"] as! Int
+        hourlyTime = dateStringFromUnixtime(hourlyTimeIntValue)
+        //        let iconString = hourlyWeather[index]["icon"]as! String
+        //        icon = weatherIconFromString(iconString)
+    }
+}
--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -21,27 +21,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var currentWeatherTemp: UILabel!
     @IBOutlet weak var currentWeatherImageView: UIImageView!
     
-    @IBOutlet weak var weeklyWeatherDay1Temp: UILabel!
-    @IBOutlet weak var weeklyWeatherDay1TempImageView:UIImageView!
-    
-    @IBOutlet weak var weeklyWeatherDay2Temp: UILabel!
-    @IBOutlet weak var weeklyWeatherDay2TempImageView:UIImageView!
-    
-    @IBOutlet weak var weeklyWeatherDay3Temp: UILabel!
-    @IBOutlet weak var weeklyWeatherDay3TempImageView:UIImageView!
-    
-    @IBOutlet weak var weeklyWeatherDay4Temp: UILabel!
-    @IBOutlet weak var weeklyWeatherDay4TempImageView:UIImageView!
-    
-    @IBOutlet weak var weeklyWeatherDay5Temp: UILabel!
-    @IBOutlet weak var weeklyWeatherDay5TempImageView:UIImageView!
-    
-    @IBOutlet weak var weeklyWeatherDay6Temp: UILabel!
-    @IBOutlet weak var weeklyWeatherDay6TempImageView:UIImageView!
-    
-    @IBOutlet weak var weeklyWeatherDay7Temp: UILabel!
-    @IBOutlet weak var weeklyWeatherDay7TempImageView:UIImageView!
-    
     
     
     override func viewDidLoad() {
@@ -79,55 +58,23 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                 do {
                     weatDictionary = try NSJSONSerialization.JSONObjectWithData(data, options: []) as! NSDictionary
                     let currentWeather = CurrentWeather(weatherDictionary: weatDictionary)
-                    let weeklyWeather = WeeklyWeather(weatherDictionary: weatDictionary)
-                    print(weatDictionary)
+                    var hourlyWeatherArray = [HourlyWeatherItem]()
+                    if let hourlyWeather = weatDictionary["hourly"]!["data"] as? NSArray {
+                        for i in 0...(hourlyWeather.count - 1) {
+                            let item = HourlyWeatherItem(hourlyWeather: hourlyWeather, index: i)
+                            hourlyWeatherArray.append(item)
+                        }
+                    }
                     
- // CURRENT WEATHER
+                    //let weeklyWeather = WeeklyWeather(weatherDictionary: weatDictionary)
+                    //print(weatDictionary)
                     
+
                     self.currentWeatherTemp.text = String(currentWeather.temperature)
                     self.currentWeatherTemp.alpha = 1.0
                     self.currentWeatherImageView.image = UIImage(named: "")
-// WEATHER DAY 1
-                    
-                    self.weeklyWeatherDay1Temp.text = "\(String(weeklyWeather.dayOneTemperatureMin)) - \(String(weeklyWeather.dayOneTemperatureMax))"
-                    self.weeklyWeatherDay1TempImageView.image = UIImage(named: "")
-                    self.weeklyWeatherDay1TempImageView.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
-                
-                    
-// WEATHER DAY 2
-                    
-                    self.weeklyWeatherDay2Temp.text = "\(String(weeklyWeather.dayTwoTemperatureMin)) - \(String(weeklyWeather.dayTwoTemperatureMax))"
-                    self.weeklyWeatherDay2TempImageView.image = UIImage(named: "")
-                    self.weeklyWeatherDay2TempImageView.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
-                    
-                    
-// WEATHER DAY 3
-                    
-                    self.weeklyWeatherDay3Temp.text = "\(String(weeklyWeather.dayThreeTemperatureMin))-\(String(weeklyWeather.dayThreeTemperatureMax))"
-                    self.weeklyWeatherDay3TempImageView.image = UIImage(named: "")
-                    self.weeklyWeatherDay3TempImageView.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
-                    
-// WEATHER DAY 4
-                    
-                    
-                    self.weeklyWeatherDay4Temp.text = "\(String(weeklyWeather.dayFourTemperatureMin))-\(String(weeklyWeather.dayFourTemperatureMax))"
-                    self.weeklyWeatherDay4TempImageView.image = UIImage(named: "")
-                    self.weeklyWeatherDay4TempImageView.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
-                    
-                    
-// WEATHER DAY 5
-                    
-                    
-                    self.weeklyWeatherDay5Temp.text = "\(String(weeklyWeather.dayFiveTemperatureMin))-\(String(weeklyWeather.dayFiveTemperatureMax))"
-                    self.weeklyWeatherDay5TempImageView.image = UIImage(named: "")
-                    self.weeklyWeatherDay5TempImageView.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
-                    
-                    
-// WEATHER DAY 6
-                    
-                    self.weeklyWeatherDay6Temp.text = "\(String(weeklyWeather.daySixTemperatureMin))-\(String(weeklyWeather.daySixTemperatureMax))"
-                    self.weeklyWeatherDay6TempImageView.image = UIImage(named: "")
-                    self.weeklyWeatherDay6TempImageView.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+
+
 
                     
                     
